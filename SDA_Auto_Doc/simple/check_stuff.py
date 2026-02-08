5f2ffefe772bafccf54020310cf76669eca69ebd
Hassan Farooq <hassanisan2026@gmail.com>
2026-02-08 12:39:58 +0500

    Delete SDA_Auto_Doc directory

--- SDA_Auto_Doc/simple/check_stuff.py
+++
@@ -1,3 +0,0 @@
-def is_even(n):
-    """Checks if a number is even."""
-    return n % 2 == 0
--- SDA_Auto_Doc/simple/math_stuff.py
+++
@@ -1,7 +0,0 @@
-def add_numbers(a, b):
-    """Adds two numbers together."""
-    return a + b
-
-def multiply(x, y):
-    """Multiplies two values."""
-    return x * y
